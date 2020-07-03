f103ba0788f740b45b0cd6a20150867d8490fdac
root <root@localhost.localdomain>
2020-07-03 17:53:49 +0800

    rpc client 調用 OnNotifyTicks

--- services/quote_service/okex_tick_quote.py
+++ services/quote_service/okex_tick_quote.py
@@ -8,11 +8,48 @@ import requests
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+import quote_pb2
+import quote_pb2_grpc
+
+#from logger import getJSONLogger
+#logger = getJSONLogger('okex-quote-client')
+
+from opencensus.trace.tracer import Tracer
+from opencensus.trace.exporters import stackdriver_exporter
+from opencensus.trace.ext.grpc import client_interceptor
+
+
 # 當前okex市場所有合約
 __builtins__.cur_fu_instruments = []
 __builtins__.cur_op_instruments = []
 
+hostPort = 'localhost:50051'
+try:
+    exporter = stackdriver_exporter.StackdriverExporter()
+    tracer = Tracer(exporter=exporter)
+    tracer_interceptor = client_interceptor.OpenCensusClientInterceptor(tracer, host_port=hostPort)
+except:
+    tracer_interceptor = client_interceptor.OpenCensusClientInterceptor()
 
+# 存合約市場深度
+def cb_save_instr_market_data():
+    channel = grpc.insecure_channel(hostPort)
+    channel = grpc.intercept_channel(channel, tracer_interceptor)
+    stub = quote_pb2_grpc.QuoteServiceStub(channel)
+    try:
+        response = stub.OnNotifyTicks(quote_pb2.Tick(
+            symbol = jsonObj['instrument_id'],
+            timestamp = str(jsonObj['timestamp']),
+            best_bid_price = str(jsonObj['best_bid']),
+            best_bid_amount = str(jsonObj['best_bid_size']),
+            best_ask_price = str(jsonObj['best_ask']),
+            best_ask_amount = str(jsonObj['best_ask_size'])
+        ))
+        #logger.info('Request sent.')
+    except grpc.RpcError as err:
+        #logger.error(err.details())
+        #logger.error('{}, {}'.format(err.code().name, err.code().value))
+        print(err)
 
 
 def fetch_all_instruments():
@@ -47,15 +84,15 @@ def fetch_tick():
         response = requests.get(getdepth_req,headers=headers, timeout=100)
         if('200' in str(response)):
             jsonObj = json.loads(response.text)
-            #cb_save_instr_market_data_mongo(jsonObj)
-    
+            cb_save_instr_market_data(jsonObj)
+
     
     for inst in __builtins__.cur_op_instruments:
         getdepth_req = "https://www.okex.com/api/option/v3/instruments/BTC-USD/summary/"+inst
         response = requests.get(getdepth_req,headers=headers, timeout=100)
         if('200' in str(response)):
             jsonObj = json.loads(response.text)
-            #cb_save_instr_market_data_mongo(jsonObj)
+            cb_save_instr_market_data(jsonObj)
     
 
 
